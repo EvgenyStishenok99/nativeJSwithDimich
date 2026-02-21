ac6e2c1588820136dd8761ac8cc2cbcbe8a8f586
EvgenyStishenok99 <stishenok999@mail.ru>
2026-02-21 16:31:12 +0300

    The project was launched on Vite. Tests can be done using vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
+
+
+
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,      // позволяет использовать test/expect без импорта
+    environment: 'jsdom' // эмулируем браузер
+  }
 })
