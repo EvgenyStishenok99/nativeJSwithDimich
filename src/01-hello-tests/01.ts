7c97b68ddf9d9eff497f90dfa65ddfc8a922db3f
EvgenyStishenok99 <stishenok999@mail.ru>
2026-02-22 15:33:30 +0300

    comlite lesson about tests

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,1 +1,20 @@
-const sentense = "Hello my friends!"
+
+export function sum (a:number, b:number) {
+  return a + b;
+}
+
+export function mult (a:number, b:number) {
+  return a * b;
+}
+
+
+
+
+ export function splitIntoWords(sentense: string) {
+
+  const words =  sentense.toLowerCase().split(" ")
+
+  return words.filter(w=>w !== "" ).map(w => w.replace("!",""))
+}
+
+
